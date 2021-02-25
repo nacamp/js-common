acfc0daddea14fef2d207e80c1cf7e55d3b3e1e8
najimmy <najimmy@gmail.com>
2021-02-25 19:15:35 +0900

    change dependencies with devDependencies

--- src/sample/hello.js
+++ src/sample/hello.js
@@ -2,7 +2,7 @@ const _ = require('lodash');
 
 const print = () =>{
   console.log( _.join(['a', 'b', 'c'], '~'));
-  console.log('v0.0.4');
+  console.log('v0.0.6');
 }
 module.exports = {
   print,
